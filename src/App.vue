462a67000d4bc2d2a4a267c2a224a84ef03c8214
idomshi <56523771+idomshi@users.noreply.github.com>
2021-12-24 06:29:34 +0900

    文字だけページを作成しました。

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,41 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
-</template>
+<div class="bg-white relative w-full h-screen py-12">
+  <header>
+  </header>
+  <main >
+    <section class="container px-5 py-24 mx-auto w-3/4 flex flex-col text-center border rounded-xl shadow">
+      <h1 class="text-3xl w-" id="新作ゲーム制作仮決定">新作ゲーム制作（仮）決定！</h1>
+      <h2 class="text-xl mt-8" id="続報を待て">続報を待て</h2>
+    </section>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+    <section class="container px-5 py-5 mx-auto flex flex-col text-center">
+      <h2 class="text-xl underline">
+        作品情報
+      </h2>
+      <ul class="mt-5">
+        <li>製作陣：みが市メンバー有志（募集中）</li>
+        <li>作品タイトル：未定</li>
+        <li>ジャンル：未定</li>
+        <li>製作期間：未定</li>
+        <li>キービジュアル：未定</li>
+        <li>対応プラットフォーム：未定</li>
+      </ul>
+    </section>
+
+    <section class="container px-5 py-5 mx-auto flex flex-col text-center">
+      <h2 class="text-xl underline" id="新着情報">新着情報</h2>
+      <ul class="mt-5">
+        <li>製作チームメンバー募集中</li>
+      </ul>
+    </section>
+  </main>
+  <footer class="text-gray-600 bod-font">
+    <div class="container px-5 py-8 mx-auto flex items-center flex-col">
+      <p>copyright miga-city</p>
+    </div>
+  </footer>
+</div>
+</template>
