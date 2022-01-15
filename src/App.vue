7cd22b5d01fc72b5c0b7e00a3891b1ebedd0eff4
idomshi <56523771+idomshi@users.noreply.github.com>
2022-01-15 12:21:54 +0900

    なんとなく画像を読み込んでみました。

--- src/App.vue
+++ src/App.vue
@@ -16,59 +16,40 @@ main, footer {
 </style>
 
 <template>
-  <bgm-control></bgm-control>
-  <div class="bg-white relative w-full h-screen">
-    <header class="h-full">
-      <section class="title flex flex-col text-center">
-        <img class="absolute h-full min-w-full object-cover object-center" :src="conceptArt" />
-        <h1 class="mt-16 z-10 text-3xl" id="新作ゲーム制作決定">新作ゲーム制作決定！</h1>
-        <h2 class="z-10 text-xl mt-4" id="続報を待て">製作チームメンバー募集中</h2>
-      </section>
-      <section class="container mt-6 px-5 py-5 mx-auto flex flex-col text-center">
-        <h2 class="z-10 text-xl underline" id="新着情報">新着情報</h2>
-        <ul class="z-10 mt-5">
-          <li><span class="px-2 text-xs">2021/12/25</span>コンセプトアートを公開しました。</li>
-        </ul>
-      </section>
-    </header>
-    <main>
-      <section class="container px-5 py-5 mx-auto flex flex-col text-center">
-        <h2 class="text-xl underline">作品情報</h2>
-        <table class="mt-5">
-          <tbody class="border-b border-gray-200">
-            <tr class="border-t border-gray-200">
-              <td class="text-left bg-gray-100 px-2">制作陣</td>
-              <td class="text-left px-2">みが市メンバー有志（募集中）</td>
-            </tr>
-            <tr class="border-t border-gray-200">
-              <td class="text-left bg-gray-100 px-2">作品タイトル</td>
-              <td class="text-left px-2">未定</td>
-            </tr>
-            <tr class="border-t border-gray-200">
-              <td class="text-left bg-gray-100 px-2">ジャンル</td>
-              <td class="text-left px-2">ノベルゲーム（フリーゲーム）</td>
-            </tr>
-            <tr class="border-t border-gray-200">
-              <td class="text-left bg-gray-100 px-2">制作期間</td>
-              <td class="text-left px-2">目安として3ヶ月程度</td>
-            </tr>
-            <tr class="border-t border-gray-200">
-              <td class="text-left bg-gray-100 px-2">対応プラットフォーム</td>
-              <td class="text-left px-2">ブラウザ</td>
-            </tr>
-            <tr class="border-t border-gray-200">
-              <td class="text-left bg-gray-100 px-2">公開方法</td>
-              <td class="text-left px-2">ノベルゲーム投稿サイトや専用ウェブサイトで一般公開</td>
-            </tr>
-          </tbody>
-        </table>
-      </section>
+  <div>
+    <header class="main-header">
+      <div class="logo-container">
+        <img id="logo" src="https://placehold.jp/24/3d4070/ffffff/250x71.jpg?text=%E6%B7%B1%E6%B7%B5%E3%81%AE%E3%82%A2%E3%83%B3%E3%83%89%E3%83%AD%E3%83%A1%E3%83%80" />
+        <bgm-control class="bgm-control" />
+      </div>
+    <div class="header-image">
+      <p>hoge</p>
+    </div>
 
-    </main>
-    <footer class="text-gray-600 bod-font">
-      <div class="container px-5 py-8 mx-auto flex items-center flex-col">
+    </header>
+    <section>
+      <h2 class="sectionTitle">title</h2>
+      <img src="https://placehold.jp/320x180.jpg" />
+      <img src="https://placehold.jp/320x180.jpg" />
+    </section>
+    <footer class="">
+      <div class="">
         <p>copyright miga-city</p>
       </div>
     </footer>
   </div>
 </template>
+
+<style scoped>
+.logo-container {
+  position: absolute;
+}
+.bgm-control {
+  position: absolute;
+}
+.header-image {
+  width: 640px;
+  height: 360px;
+  background-image: url('https://placehold.jp/96/70739e/ffffff/640x360.png?text=%E3%83%98%E3%83%83%E3%83%80%E3%83%BC%E7%94%BB%E5%83%8F');
+}
+</style>
