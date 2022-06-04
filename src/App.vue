122b98b795df49ff8935d5e2769d0c422b2ca9cd
idomshi <56523771+idomshi@users.noreply.github.com>
2022-06-04 23:54:42 +0900

    文章のたたき台を入力しました。

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,6 @@ import BgmControl from './components/BgmControl.vue';
   font-family: "Noto Serif JP", serif;
   font-weight: 400;
 }
-
 </style>
 
 <template>
@@ -34,11 +33,58 @@ import BgmControl from './components/BgmControl.vue';
 
       <section>
         <h2 class="flex justify-center text-2xl">ストーリー</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque quasi inventore, tenetur reiciendis
-          tempora
-          voluptas perferendis eligendi. Commodi amet iure, nesciunt natus assumenda minus vitae. Maiores at quis
-          tempore
-          magni?</p>
+        <p class="w-1/2 mx-auto">
+          人類は滅びた<br />
+          <br />
+          それに伴い自然はかつての豊かさを取り戻し<br />
+          <br />
+          消えたはずの動物たちも数を増やし<br />
+          <br />
+          人によって滅ぼされた命の息吹は<br />
+          <br />
+          回復の兆しを見せつつあった<br />
+          <br />
+          <br />
+          そんな中、無機質な機械音だけが鳴り響く一室に<br />
+          <br />
+          一人の青年がいた<br />
+          <br />
+          正確に言うと但し滅びたはずの人間ではなく<br />
+          <br />
+          青年の姿をしたアンドロイドである<br />
+          <br />
+          彼が見つめる先にあるのは４つのカプセル<br />
+          <br />
+          中にいるのは4人の女性<br />
+          <br />
+          <br />
+          <br />
+          これは<br />
+          <br />
+          <br />
+          <br />
+          人とアンドロイドが紡ぐ<br />
+          <br />
+          苦しくも儚いどうしようもなく終わってしまった物語
+        </p>
+      </section>
+
+      <section>
+        <div class="w-1/2 mx-auto">
+          <h2 class="flex justify-center text-2xl">舞台設定</h2>
+          <h3 class="flex justify-center text-xl">研究所</h3>
+          <p>本作の主人公ロイドが暮らす研究所。博士のほか数人が永い眠りについているようだ。周辺に森や海がある。</p>
+          <h3 class="flex justify-center text-xl">塔</h3>
+          <p>そびえたつ塔。何かの施設らしい。</p>
+          <!-- <h3 class="flex justify-center text-xl">海</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque quasi inventore, tenetur reiciendis
+            tempora
+            </p> -->
+          <h3 class="flex justify-center text-xl">商店街・学校</h3>
+          <p>既に使われていない。</p>
+          <!-- <h3 class="flex justify-center text-xl">温泉</h3>
+          <p>温かいお湯に身体を浸けると疲れが取れる。</p> -->
+        </div>
       </section>
 
       <section class="flex flex-col gap-4">
@@ -47,47 +93,36 @@ import BgmControl from './components/BgmControl.vue';
         <img src="https://placehold.jp/320x180.jpg" />
       </section>
 
-      <section>
+      <section class="characters flex flex-col gap-2">
         <h2 class="flex justify-center text-2xl">キャラクター</h2>
 
         <div>
-          <h3>博士</h3>
-          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore, odit quae expedita voluptatibus dolore
-            nobis, facere voluptates reprehenderit illum, repellendus neque aspernatur eum itaque nam. Architecto,
-            dicta.
-            Tenetur, voluptas earum!</p>
+          <h3 class="text-xl">博士</h3>
+          <p>コールドスリープ施設の研究者。ロイドの生みの親。1000年以上寝てたけどまた寝た。</p>
         </div>
 
         <div>
-          <h3>ロイド</h3>
-          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore, odit quae expedita voluptatibus dolore
-            nobis, facere voluptates reprehenderit illum, repellendus neque aspernatur eum itaque nam. Architecto,
-            dicta.
-            Tenetur, voluptas earum!</p>
+          <h3 class="text-xl">ロイド</h3>
+          <!-- <p>アンドロイド。個性的な少女たちと上手くコミュニケーションが取れるか不安。</p> -->
+          <p>1000年以上コールドスリープされていた博士たちを守っていたイケメン。<br />
+            博士に最高傑作と言われ、コーヒーを入れるのがうまいといわれるイケメン。<br />
+            なんなら博士に抱きしめてさえもらえるイケメン。<br />
+            しつこいようだがイケメン。</p>
         </div>
 
         <div>
-          <h3>りな</h3>
-          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore, odit quae expedita voluptatibus dolore
-            nobis, facere voluptates reprehenderit illum, repellendus neque aspernatur eum itaque nam. Architecto,
-            dicta.
-            Tenetur, voluptas earum!</p>
+          <h3 class="text-xl">りな</h3>
+          <p>姉のりかもロイドと会ったことがあるようで……？</p>
         </div>
 
         <div>
-          <h3>アトリア</h3>
-          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore, odit quae expedita voluptatibus dolore
-            nobis, facere voluptates reprehenderit illum, repellendus neque aspernatur eum itaque nam. Architecto,
-            dicta.
-            Tenetur, voluptas earum!</p>
+          <h3 class="text-xl">アトリア</h3>
+          <p>無邪気で元気な少女。小柄でよく跳びはねる。はらぺこ。</p>
         </div>
 
         <div>
-          <h3>継</h3>
-          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore, odit quae expedita voluptatibus dolore
-            nobis, facere voluptates reprehenderit illum, repellendus neque aspernatur eum itaque nam. Architecto,
-            dicta.
-            Tenetur, voluptas earum!</p>
+          <h3 class="text-xl">継</h3>
+          <p>ゴールド免許所持者。</p>
         </div>
       </section>
 
@@ -206,5 +241,13 @@ import BgmControl from './components/BgmControl.vue';
 ul {
   @apply mx-8;
   @apply list-disc;
+}
+
+main>section {
+  @apply bg-gray-50;
+}
+
+.characters div {
+  @apply w-1/2 mx-auto;
 }
 </style>
